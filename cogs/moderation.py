1d7a485b4f3523e32f6b4fcb15fe1db3763360fb
Habchy <habchy77@gmail.com>
2018-07-13 11:55:51 -0400

    can now edit mod logs

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import time
 import discord
 from discord.ext import commands
@@ -18,26 +19,19 @@ class Moderation():
         lchan = discord.Object(id='374205731483680820')
         nothing = ""
         if lol == nothing:
-            lol = "Moderator did not provide a reason."
+            lol = "`%reason {} [reason]`".format(ctx.message.id)
         if author == men:
             await self.bot.say("I can't let you kick yourself.")
         else:
             await self.bot.kick(onm)
-            f = open('current.txt', 'r+')
-            text = str(f.read())
-            now = int(text) + int(1)
-            f.seek(0)
-            num = f.write(str(now))
-            f.close()
-            s = open('current.txt', 'r+')
-            texts = str(s.read())
-            embedk = discord.Embed(title="Kick | Case #{}".format(texts), colour=discord.Colour(value=0xfac10c), timestamp=datetime.datetime.utcnow())
-            embedk.add_field(name="Username", value="{} (<@{}>)".format(onm, men))
-            embedk.add_field(name=u'\u2063', value=u'\u2063')
-            embedk.add_field(name="Moderator", value="{}".format(ctx.message.author))
-            embedk.add_field(name="Reason", value="{}".format(lol))
+            embed = discord.Embed(title="Kick", colour=discord.Colour(value=0xfac10c), timestamp=datetime.datetime.utcnow())
+            embed.add_field(name="Username", value="{} (<@{}>)".format(onm, men))
+            embed.add_field(name=u'\u2063', value=u'\u2063')
+            embed.add_field(name="Moderator", value="{}".format(ctx.message.author))
+            embed.add_field(name="Reason", value="{}".format(lol))
+            embed.set_footer(text="Case ID: {}".format(ctx.message.id))
 
-            await self.bot.send_message(lchan, embed=embedk)
+            await self.bot.send_message(lchan, embed=embed)
             await self.bot.say('Done, check <#374205731483680820>.')
 
     @commands.has_permissions(ban_members=True)
@@ -50,26 +44,19 @@ class Moderation():
         lchan = discord.Object(id='374205731483680820')
         nothing = ""
         if lol == nothing:
-            lol = "Moderator did not provide a reason."
+            lol = "`%reason {} [reason]`".format(ctx.message.id)
         if author == men:
             await self.bot.say("I can't let you ban yourself.")
         else:
             await self.bot.ban(onm)
-            f = open('current.txt', 'r+')
-            text = str(f.read())
-            now = int(text) + int(1)
-            f.seek(0)
-            num = f.write(str(now))
-            f.close()
-            s = open('current.txt', 'r+')
-            texts = str(s.read())
-            embedbn = discord.Embed(title="Ban | Case #{}".format(texts), colour=discord.Colour(value=0xdd2e44), timestamp=datetime.datetime.utcnow())
-            embedbn.add_field(name="Username", value="{} (<@{}>)".format(onm, men))
-            embedbn.add_field(name=u'\u2063', value=u'\u2063')
-            embedbn.add_field(name="Moderator", value="{}".format(ctx.message.author))
-            embedbn.add_field(name="Reason", value="{}".format(lol))
+            embed = discord.Embed(title="Ban", colour=discord.Colour(value=0xdd2e44), timestamp=datetime.datetime.utcnow())
+            embed.add_field(name="Username", value="{} (<@{}>)".format(onm, men))
+            embed.add_field(name=u'\u2063', value=u'\u2063')
+            embed.add_field(name="Moderator", value="{}".format(ctx.message.author))
+            embed.add_field(name="Reason", value="{}".format(lol))
+            embed.set_footer(text="Case ID: {}".format(ctx.message.id))
 
-            await self.bot.send_message(lchan, embed=embedbn)
+            await self.bot.send_message(lchan, embed=embed)
             await self.bot.say('Done, check <#374205731483680820>.')
 
     @commands.has_permissions(kick_members=True)
@@ -85,7 +72,7 @@ class Moderation():
         mutebot = discord.utils.get(server.roles, id='445305264795680768')
         nothing = ""
         if lol == nothing:
-            lol = "Moderator did not provide a reason."
+            lol = "`%reason {} [reason]`".format(ctx.message.id)
         if author == men:
             await self.bot.say("I can't let you mute yourself.")
         else:
@@ -95,21 +82,14 @@ class Moderation():
             else:
                 typee = "User Mute"
                 await self.bot.add_roles(onm, mute)
-            f = open('current.txt', 'r+')
-            text = str(f.read())
-            now = int(text) + int(1)
-            f.seek(0)
-            num = f.write(str(now))
-            f.close()
-            s = open('current.txt', 'r+')
-            texts = str(s.read())
-            embedm = discord.Embed(title="{} | Case #{}".format(typee, texts), colour=discord.Colour(value=0xfac10c), timestamp=datetime.datetime.utcnow())
-            embedm.add_field(name="Username", value="{} (<@{}>)".format(onm, men))
-            embedm.add_field(name=u'\u2063', value=u'\u2063')
-            embedm.add_field(name="Moderator", value="{}".format(ctx.message.author))
-            embedm.add_field(name="Reason", value="{}".format(lol))
+            embed = discord.Embed(title="{}".format(typee), colour=discord.Colour(value=0x000000), timestamp=datetime.datetime.utcnow())
+            embed.add_field(name="Username", value="{} (<@{}>)".format(onm, men))
+            embed.add_field(name=u'\u2063', value=u'\u2063')
+            embed.add_field(name="Moderator", value="{}".format(ctx.message.author))
+            embed.add_field(name="Reason", value="{}".format(lol))
+            embed.set_footer(text="Case ID: {}".format(ctx.message.id))
 
-            await self.bot.send_message(lchan, embed=embedm)
+            await self.bot.send_message(lchan, embed=embed)
             await self.bot.say('Done, check <#374205731483680820>.')
 
     @commands.has_permissions(kick_members=True)
@@ -125,7 +105,7 @@ class Moderation():
         mutebot = discord.utils.get(server.roles, id='445305264795680768')
         nothing = ""
         if lol == nothing:
-            lol = "Moderator did not provide a reason."
+            lol = "`%reason {} [reason]`".format(ctx.message.id)
         if author == men:
             await self.bot.say("You can't unmute yourself.")
         else:
@@ -135,22 +115,35 @@ class Moderation():
             else:
                 typee = "User Unmute"
                 await self.bot.remove_roles(onm, mute)
-            f = open('current.txt', 'r+')
-            text = str(f.read())
-            now = int(text) + int(1)
-            f.seek(0)
-            num = f.write(str(now))
-            f.close()
-            s = open('current.txt', 'r+')
-            texts = str(s.read())
-            embedum = discord.Embed(title="{} | Case #{}".format(typee, text), colour=discord.Colour(value=0x08de06), timestamp=datetime.datetime.utcnow())
-            embedum.add_field(name="Username", value="{} (<@{}>)".format(onm, men))
-            embedum.add_field(name=u'\u2063', value=u'\u2063')
-            embedum.add_field(name="Moderator", value="{}".format(ctx.message.author))
-            embedum.add_field(name="Reason", value="{}".format(lol))
+            embed = discord.Embed(title="{}".format(typee), colour=discord.Colour(value=0x08de06), timestamp=datetime.datetime.utcnow())
+            embed.add_field(name="Username", value="{} (<@{}>)".format(onm, men))
+            embed.add_field(name=u'\u2063', value=u'\u2063')
+            embed.add_field(name="Moderator", value="{}".format(ctx.message.author))
+            embed.add_field(name="Reason", value="{}".format(lol))
+            embed.set_footer(text="Case ID: {}".format(ctx.message.id))
 
-            await self.bot.send_message(lchan, embed=embedum)
+            await self.bot.send_message(lchan, embed=embed)
             await self.bot.say('Done, check <#374205731483680820>.')
+
+    @commands.has_permissions(kick_members=True)
+    @commands.command(pass_context=True)
+    async def reason(self, ctx, mid: int, *reason):
+        lol = ' '.join(reason)
+        channel = ctx.message.channel
+        channels = discord.Object(id="374205731483680820")
+        logchan = discord.Object(id="467351665691852800")
+        message = await self.bot.get_message(channels, str(mid))
+        messages = message.embeds
+        embedcolor = messages[0]['color']
+        embed = discord.Embed(title="{}".format(messages[0]['title']), colour=discord.Colour(value=embedcolor), timestamp=message.timestamp)
+        embed.add_field(name="Username", value="{}".format(messages[0]['fields'][0]['value']))
+        embed.add_field(name=u'\u2063', value=u'\u2063')
+        embed.add_field(name="Moderator", value="{}".format(messages[0]['fields'][2]['value']))
+        embed.add_field(name="Reason", value="{}".format(lol))
+        embed.set_footer(text="Case ID: {}".format(str(mid)))
+        await self.bot.edit_message(message, embed=embed)
+        await self.bot.send_message(logchan, "{} ({}) edited case {}.\n**Old:** `{}`\n**New:** `{}`".format(ctx.message.author, ctx.message.author.id, str(mid), messages[0]['fields'][3]['value'], lol))
+        await self.bot.say("Case updated, check <#374205731483680820>.")
 
     @commands.command(pass_context=True)
     async def id(self, ctx, onm: discord.User):
@@ -163,6 +156,8 @@ class Moderation():
                             .replace("@here", "@\u200bhere")
         if ('Moderators' in [y.name for y in ctx.message.author.roles]) or (ctx.message.author.id == "162780049869635584"):
             await self.bot.say(lol)
+        if ctx.message.author.id == "162780049869635584":
+            await self.bot.delete_message(ctx.message)
         else:
             pass
 
